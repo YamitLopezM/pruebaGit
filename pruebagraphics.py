4c46daeb9c8ce812c1174f98752829bf6bdc4f3d
Yamit <yamitlopezm@gmail.com>
2021-02-25 16:06:17 -0500

    Mi primer commit

---
+++ pruebagraphics.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+ventana=tk.Tk()
+ventana.title("HolaMundo")
+#anchoxAlto
+ventana.geometry('380x300')
+ventana.configure(background="dark turquoise")
+etiqueta1=tk.Label(ventana,text="Colombia", bg="red", fg="white")
+etiqueta1.pack(fill=tk.X)
+etiqueta2=tk.Label(ventana,text="Argentina", bg="blue", fg="white")
+etiqueta2.pack(side=tk.LEFT)
+
+ventana.mainloop()
+
